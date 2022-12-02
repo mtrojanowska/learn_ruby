bcc500b1bb0300c51414b8c358e5bfdf07bb430d
Marta Trojanowska <m.troyanovska@gmail.com>
2022-12-02 12:17:12 +0100

    authentication of teachers with devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,23 @@
 # frozen_string_literal: true
 
 class ApplicationController < ActionController::Base
+<<<<<<< HEAD
   def new; end
 
   def create; end
+=======
+  protect_from_forgery with: :exception, prepend: true
+  before_action :authenticate_teacher!
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: %i[first_name last_name email password password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_in, keys: %i[email password])
+    devise_parameter_sanitizer.permit(:account_update,
+                                      keys: %i[first_name last_name email password current_password
+                                               password_confirmation])
+  end
+>>>>>>> authentication of teachers with devise
 end
