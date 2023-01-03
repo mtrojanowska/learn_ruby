115a318f973f94f69bae000bf7f9cb3ef1f11caa
Marta Trojanowska <m.troyanovska@gmail.com>
2023-01-03 18:16:52 +0100

    commented out code removed from factories/teachers.rb

--- spec/factories/teachers.rb
+++ spec/factories/teachers.rb
@@ -7,8 +7,6 @@ FactoryBot.define do
       Faker::Name.last_name
     end
     email { Faker::Internet.email }
-    # password { 'dupablada' }
-    # password_confirmation { 'dupablada' }
     password { Faker::Internet.password(min_length: 8) }
     password_confirmation { :password }
   end
