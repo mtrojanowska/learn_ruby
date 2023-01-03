42a932e40c1d0828977e293295dbeb4a8292a31b
Marta Trojanowska <m.troyanovska@gmail.com>
2023-01-03 19:04:11 +0100

    enother commit of factories/teachers.rb :(

--- spec/factories/teachers.rb
+++ spec/factories/teachers.rb
@@ -8,6 +8,6 @@ FactoryBot.define do
     end
     email { Faker::Internet.email }
     password { Faker::Internet.password(min_length: 8) }
-    password_confirmation { password }
+    password_confirmation { :password }
   end
 end
