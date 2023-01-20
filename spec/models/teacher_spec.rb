8c052192be8c28b1619317eaa651bd98475c0915
Marta Trojanowska <m.troyanovska@gmail.com>
2023-01-20 18:28:45 +0100

    rebase origin main

--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -8,7 +8,6 @@ RSpec.describe Teacher, type: :model do
     end
   end
 
-<<<<<<< HEAD
     describe 'validation success' do
       let(:teacher) { Teacher.new(attributes_for(:teacher)) }
 
@@ -24,38 +23,13 @@ RSpec.describe Teacher, type: :model do
       let(:teacher_email_dup) { Teacher.new(attributes_for(:teacher, email: teacher.email)) }
 
       it { is_expected.not_to validate_uniqueness_of(:email) }
-=======
-  describe 'validation success' do
-    let(:teacher) { Teacher.new(attributes_for(:teacher)) }
-    it { is_expected.to validate_presence_of(:first_name) }
-    it { is_expected.to validate_presence_of(:last_name) }
-    it { is_expected.to validate_presence_of(:email) }
-    it { is_expected.to validate_presence_of(:password) }
-    it { is_expected.to validate_presence_of(:password_confirmation) }
-  end
-
-  describe 'email validation fail' do
-    it 'for unique email' do
-      teacher_1 = create(:teacher, attributes_for(:teacher))
-      teacher_2 = build(:teacher, attributes_for(:teacher, email: teacher_1.email))
-      expect(teacher_2).not_to be_valid
->>>>>>> email unique validation spec fixed
     end
   end
 
-<<<<<<< HEAD
-    describe 'validation fail' do
-      let(:teacher) { Teacher.new(attributes_for(:teacher, password: 'pea')) }
-
-      it 'for password too short' do
-        expect(teacher).not_to be_valid
-      end
-=======
   describe 'validation fail' do
     let(:teacher) { Teacher.new(attributes_for(:teacher, password: 'pea')) }
     it 'for password too short' do
       expect(teacher).not_to be_valid
->>>>>>> email unique validation spec fixed
     end
   end
 end
