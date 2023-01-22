79c6f6d804e0d169afeecd1176abc2b2a63f2719
Marta Trojanowska <m.troyanovska@gmail.com>
2023-01-22 17:33:56 +0100

    branch after rebase

--- spec/models/teacher_spec.rb
+++ spec/models/teacher_spec.rb
@@ -8,26 +8,26 @@ RSpec.describe Teacher, type: :model do
     end
   end
 
-    describe 'validation success' do
-      let(:teacher) { Teacher.new(attributes_for(:teacher)) }
+  describe 'validation success' do
+    let(:teacher) { Teacher.new(attributes_for(:teacher)) }
 
-      it { is_expected.to validate_presence_of(:first_name) }
-      it { is_expected.to validate_presence_of(:last_name) }
-      it { is_expected.to validate_presence_of(:email) }
-      it { is_expected.to validate_presence_of(:password) }
-    end
-
-    describe 'email uniqeness fail' do
-      let(:teacher) { Teacher.new(attributes_for(:teacher)) }
-      let(:teacher_email_dup) { Teacher.new(attributes_for(:teacher, email: teacher.email)) }
-
-      it { is_expected.not_to validate_uniqueness_of(:email) }
-    end
+    it { is_expected.to validate_presence_of(:first_name) }
+    it { is_expected.to validate_presence_of(:last_name) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_presence_of(:password) }
   end
 
-  describe 'validation fail' do
+  describe 'email uniqeness fail' do
+    let(:teacher) { Teacher.new(attributes_for(:teacher)) }
+    let(:teacher_email_dup) { Teacher.new(attributes_for(:teacher, email: teacher.email)) }
+
+    it { is_expected.not_to validate_uniqueness_of(:email) }
+  end
+
+  describe 'password validation' do
     let(:teacher) { Teacher.new(attributes_for(:teacher, password: 'pea')) }
-    it 'for password too short' do
+
+    it 'fails for less than 6 characters' do
       expect(teacher).not_to be_valid
     end
   end
