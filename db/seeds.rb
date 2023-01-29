c7c08f1d2aef9447adb0a75cfe4025db6ec89b92
Marta Trojanowska <m.troyanovska@gmail.com>
2023-01-29 19:49:37 +0100

    seeds modified

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,23 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-5.times do
-    teacher = Teacher.create!(first_name: Faker::Name.first_name, last_name: Faker::Name.last_name,
-        email: Faker::Internet.email, password: Faker::Internet.password(min_length: 6))
-end
+Teacher.delete_all
+
+    teachers = Teacher.create!(
+                            [{
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        email: 'random@example.com', 
+        password: 'dupablada'},
+                               {
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        email: 'random1@example.com', 
+        password: 'dupablada'},
+                               {
+        first_name: Faker::Name.first_name,
+        last_name: Faker::Name.last_name,
+        email: 'random3@example.com', 
+        password: 'dupablada'}],
+    )
+    
