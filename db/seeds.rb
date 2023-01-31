593735e154fd72ffe971d211584b3249c100014f
Marta Trojanowska <m.troyanovska@gmail.com>
2023-01-31 20:40:19 +0100

    delete_all in seed.rb removed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,28 +1,30 @@
-# frozen_string_literal: true
+# frozen_string_literal= true
 # This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
+# The data can then be loaded with the bin/rails db=seed command (or created alongside the database with db:setup).
 #
 # Examples:
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
-Teacher.delete_all
 
-    teachers = Teacher.create!(
-                            [{
+   
+    teachers = Teacher.create!( 
+                  [ {     
         first_name: Faker::Name.first_name,
         last_name: Faker::Name.last_name,
         email: 'random@example.com', 
         password: 'dupablada'},
-                               {
+                       {      
         first_name: Faker::Name.first_name,
         last_name: Faker::Name.last_name,
         email: 'random1@example.com', 
-        password: 'dupablada'},
-                               {
+        password: 'dupablada1'},
+                            {
         first_name: Faker::Name.first_name,
         last_name: Faker::Name.last_name,
         email: 'random3@example.com', 
-        password: 'dupablada'}],
-    )
+        password: 'dupablada3'}]
+                            ).find_or_create_by!(email: teacher[:email])
+    
+    
     
